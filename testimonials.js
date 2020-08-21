d0bcfe855ab735c7765a6c822613aeace20ea85a
Michael Bade <mbade314@gmail.com>
2020-08-21 18:51:07 -0500

    First commit

---
+++ testimonials.js
@@ -0,0 +1,11 @@
+var testimonials = new Array("I love Superb Roofing! - Sarah", "Couldn't be more satisfied. - Ryan"); 
+var tnum = 0; 
+var tttl = testimonials.length;
+
+function rotate() { if (++tnum > tttl-1) tnum=0;
+    let quote = document.getElementById("testimonials-rotation")
+    quote.innerHTML = testimonials[tnum].italics();
+    
+    setTimeout("rotate()", 4000); 
+} window.onload = rotate();
+
